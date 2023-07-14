eb25f8727b5a7e85b4cde7261bcb0988096dc72a
vatsalpatel2503 <vatsalpansuriya25@gmail.com>
2023-07-14 08:57:47 +0530

    This is my FIRST assign commit

---
+++ FACT.py
@@ -0,0 +1,9 @@
+#write a program where number is given by the user and FACTORIAL shuld be shown.
+
+num=int(input("ENTER THE NUMBER : "));
+if x < 0;
+   print( " FACTORIAL NUMBER IS NOT DEFINED " )
+else :
+    x = factorial number
+   print( "FACTORIAL OF NUMBER " )
+
